f22adf393408c89068ae59c109a3716ae2ce9b12
shiqui <44322197+shiqui@users.noreply.github.com>
2024-02-08 11:49:28 -0500

    Refactor interactionCreate event handler

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,33 +3,36 @@ const { Events } = require('discord.js')
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-		// ignore non-command interactions and non-modal submissions
-		if (!(interaction.isChatInputCommand() || interaction.isModalSubmit())) return
-
-		// handle form submission
-		if (interaction.customId === 'texModal') {
-			const tex = interaction.fields.getTextInputValue('texInput')
-			await interaction.reply({ content: tex })
-			return
-		}
-
-		// handle slash commands
-		const command = interaction.client.commands.get(interaction.commandName)
-
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`)
-			return
-		}
-
-		try {
-			await command.execute(interaction)
-		} catch (error) {
-			console.error(error)
-			if (interaction.replied || interaction.deferred) {
-				await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true })
-			} else {
-				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
-			}
+		if (interaction.isModalSubmit()) {
+			await modalHandler(interaction)
+		} else if (interaction.isChatInputCommand()) {
+			await commandHandler(interaction)
 		}
 	},
+}
+
+const modalHandler = async (interaction) => {
+	if (interaction.customId !== 'texModal') return
+	const tex = interaction.fields.getTextInputValue('texInput')
+	await interaction.reply({ content: tex })
+}
+
+const commandHandler = async (interaction) => {
+	const command = interaction.client.commands.get(interaction.commandName)
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`)
+		return
+	}
+
+	try {
+		await command.execute(interaction)
+	} catch (error) {
+		console.error(error)
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true })
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+		}
+	}
 }
